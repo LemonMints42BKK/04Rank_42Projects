a6d0b41271e7ecc4e6637956557e09b21ffe9495
LemonMints42BKK <131885340+LemonMints42BKK@users.noreply.github.com>
2023-08-14 10:53:36 +0000

    Rank04 CPP CPP00 ex01  the program waits for another ADD or

--- Rank04/CPP/CPP00/ex01/phonebook.cpp
+++ Rank04/CPP/CPP00/ex01/phonebook.cpp
@@ -6,15 +6,35 @@
 /*   By: pnopjira <65420071@kmitl.ac.th>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 05:42:39 by pnopjira          #+#    #+#             */
-/*   Updated: 2023/08/14 08:40:31 by pnopjira         ###   ########.fr       */
+/*   Updated: 2023/08/14 10:34:03 by pnopjira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include 
+#include "phonebook.hpp"
 
 int main(void)
 {
-    
+    std::cout << ">>>>THIS IS PHONEBOOK<<<<" << std::endl;
+    std::cout << "Enter the COMMAND" << std::endl;
+    std::cout << "# ADD to contact list." << std::endl;
+    std::cout << "# SEARCH to display the contact list" << std::endl;
+    std::cout << "# EXIT program." << std::endl;
+
+    std::string cmd = "";
+    while (cmd != "EXIT")
+    {
+            std::cout << "Enter the COMMAND : ";
+            std::getline(std::cin, cmd);
+            if (cmd == "EXIT")
+                exit(0);
+            else if (cmd == "ADD")
+            {
+                std::cout << cmd << std::endl;
+            }
+            else if (cmd == "SEARCH")
+            {
+                std::cout << cmd << std::endl;
+            }
+    }
     return (0);
 }
--- Rank04/CPP/CPP00/ex01/phonebook.hpp
+++ Rank04/CPP/CPP00/ex01/phonebook.hpp
@@ -6,11 +6,17 @@
 /*   By: pnopjira <65420071@kmitl.ac.th>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 05:42:42 by pnopjira          #+#    #+#             */
-/*   Updated: 2023/08/14 08:38:43 by pnopjira         ###   ########.fr       */
+/*   Updated: 2023/08/14 09:58:55 by pnopjira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef
+#ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
+
+# include <iostream>
+# include <string>
+# include <stdlib.h>
+
+
 
 #endif
