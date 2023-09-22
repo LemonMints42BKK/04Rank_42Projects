c4bfdeff77a6b353ef984b20415f5002aa52d2e6
LemonMints42BKK <131885340+LemonMints42BKK@users.noreply.github.com>
2023-09-22 15:38:33 +0000

    finished CPPoo

---
+++ Rank04/02CPP/ex00/Fixed.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnopjira <65420071@kmitl.ac.th>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 07:02:11 by pnopjira          #+#    #+#             */
+/*   Updated: 2023/09/21 07:04:22 by pnopjira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+class Fixed
+{
+    private:
+        int _FixedPointValue;
+    public:
+        Fixed(/* args */);
+        ~Fixed();
+};
+
+#endif
