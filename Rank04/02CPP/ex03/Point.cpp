54cdfc84d80d298650b519d7c0edd2f14cb6037d
LemonMints42BKK <131885340+LemonMints42BKK@users.noreply.github.com>
2023-09-25 09:26:01 +0000

    RAnk04 02CPP

--- Rank04/02CPP/ex03/Point.cpp
+++ Rank04/02CPP/ex03/Point.cpp
@@ -6,7 +6,42 @@
 /*   By: pnopjira <65420071@kmitl.ac.th>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 10:56:07 by pnopjira          #+#    #+#             */
-/*   Updated: 2023/09/23 10:56:08 by pnopjira         ###   ########.fr       */
+/*   Updated: 2023/09/25 06:31:44 by pnopjira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Point.hpp"
+
+Point::Point(): _x(0), _y(0){
+    return ;
+}
+
+Point::Point(float const x, float const y): _x(x), _y(y)
+{
+    std::cout << "Point constructor called" << std::endl;
+    std::cout << "x: " << this->_x << std::endl;
+    std::cout << "y: " << this->_y << std::endl;
+    return ;
+}
+
+Point::Point(Point const &other){
+    *this = other;
+}
+
+Point::~Point(){
+    std::cout << "Point destructor called" << std::endl;
+    return ;
+}
+
+Point &Point::operator=(Point const &other){
+    (void)other;
+    return *this;
+}
+
+Fixed const Point::getX() const{
+    return this->_x;
+}
+
+Fixed const Point::getY() const{
+    return this->_y;
+}
--- Rank04/02CPP/ex03/main.cpp
+++ Rank04/02CPP/ex03/main.cpp
@@ -6,14 +6,26 @@
 /*   By: pnopjira <65420071@kmitl.ac.th>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 07:02:17 by pnopjira          #+#    #+#             */
-/*   Updated: 2023/09/23 10:55:10 by pnopjira         ###   ########.fr       */
+/*   Updated: 2023/09/25 09:24:01 by pnopjira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include "Point.hpp"
 #include <iostream>
 
 int main(void){
+	Point	a(0, 0);
+	Point	b(7, 5);
+	Point	c(9, 0);
+	
+	Point	p1(0, 0);
+	Point	p2(5, 2);
+	Point	p3(10, 0);
+	
+	std::cout << "P1 :" << bsp(a, b, c, p1) << std::endl;
+	std::cout << "P2 :"<< bsp(a, b, c, p2) << std::endl;
+	std::cout << "P3 :"<< bsp(a, b, c, p3) << std::endl;
 
     return 0;
 }
